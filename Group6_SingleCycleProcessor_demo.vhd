4c5964949683a4a7149600bd1a9279681cb8f228
Siddhartha Joshi <sidjos@gmail.com>
2014-11-22 01:06:36 -0600

    better demo file with clock

--- Group6_SingleCycleProcessor_demo.vhd
+++ Group6_SingleCycleProcessor_demo.vhd
@@ -20,44 +20,20 @@ component top_level is
   );
 end component;
 
-signal clock, reset_sig : std_logic;
+signal clock : std_logic:='0';
+signal reset_sig : std_logic;
 begin
 test: top_level port map (clk=>clock, reset=>reset_sig, ovf=>ovf,cout=>cout);
+clock <= not clock after 5 ns;
+
 testprocess: process begin
-clock<='0';
 reset_sig <= '1';
 wait for 5 ns;
-clock<='1';
 reset_sig <='1';
 wait for 5 ns;
-clock<='0';
 reset_sig<='0';
-wait for 5 ns;
-clock<='1';
-wait for 5 ns;
-clock<='0';
-wait for 5 ns;
-clock<='1';
-wait for 5 ns;
-clock<='0';
-wait for 5 ns;
-clock<='1';
-wait for 5 ns;
-clock<='0';
-reset_sig<='0';
-wait for 5 ns;
-clock<='1';
-wait for 5 ns;
-clock<='0';
-wait for 5 ns;
-clock<='1';
-wait for 5 ns;
-clock<='0';
-wait for 5 ns;
-clock<='1';
-
---clock <= not clock after 10 ns;
 wait;
+
 end process;
 
 end structural;
