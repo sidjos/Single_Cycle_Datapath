862f8afcbf3616baadaf0444a01c65ae10c45059
Siddhartha Joshi <sidjos@gmail.com>
2014-11-22 20:09:21 -0600

    good version of files, new memories

--- 6_b_reg_32.vhd
+++ 6_b_reg_32.vhd
@@ -22,7 +22,6 @@ end reg_32;
 
 
 architecture structural of reg_32 is
-<<<<<<< HEAD
     
 component mux_5_S is
   port (
@@ -86,76 +85,13 @@ begin
         dff36_map:	dffr	port map ( clk, Rw(4), Rw_out(4));
     
     dff37_map:	dffr	port map ( clk, RegWr, RegWr_out);
-    
-    
-	-- ra <= rs
-	-- rb <= rt
-	
-=======
 
-
-component alu is 
-   port(
-       ctrl: in std_logic_vector (3 downto 0);
-       A : in std_logic_vector(31 downto 0);
-       B: in std_logic_vector ( 31 downto 0);
-       cout: out std_logic; 
-       ovf: out std_logic; 
-       ze: out std_logic;
-       R: out std_logic_vector(31 downto 0)
-       );
-end component; 
-
-signal mux0: std_logic_vector(31 downto 0);
-signal mux1: std_logic_vector(31 downto 0);
-signal sellll, XXXXX, YYYYY: std_logic;
-signal KKKKK,PPPPP: std_logic_vector(31 downto 0);
-
-begin
-
-
-
-alu000_map: alu port map ( ctrl=>B"0001", A(4 downto 0)=>rw, A(31 downto 5)=>B"000000000000000000000000000", 
-					  B(4 downto 0)=>ra, B(31 downto 5)=>B"000000000000000000000000000",
-										ze=>XXXXX);
-
-
-alu001_map: alu port map ( ctrl=>B"0001", A(4 downto 0)=>ra, A(31 downto 5)=>B"000000000000000000000000000", 
- 					  
-					  B(31 downto 5)=>B"000000000000000000000000000", B(4 downto 0)=>rb,
-										ze=>YYYYY);
-anddd_map : and_gate port map (x=>XXXXX, y=>YYYYY, z=>sellll);
-muxxxxx:    mux_32 port map (sel=>sellll, src0=>KKKKK, src1=>PPPPP, z=>A);
-
-
-
-	      
->>>>>>> 1287bebcec77da95fbd8f87974cf3d1b5fe9c333
-		--Arrange 2 Synchronized RAM with proper connections
-	mux0_map:	mux_32	 port map (sel=>RegWr_out, src0(31 downto 5)=>B"000000000000000000000000000", src0(4 downto 0)=>rs,
-					               src1(31 downto 5)=>B"000000000000000000000000000", src1(4 downto 0)=>Rw_out, z=>mux0);
-
-	mux1_map:	mux_32	 port map (sel=>RegWr_out, src0(31 downto 5)=>B"000000000000000000000000000", src0(4 downto 0)=>rt,
-					               src1(31 downto 5)=>B"000000000000000000000000000", src1(4 downto 0)=>Rw_out, z=>mux1);
-	
-	--mux0test <= mux0; for testing
-	--mux1test <= mux1; for testing
-	
 	sram0_map:  	sram	
  			generic map (mem_file => "reg_init.dat")
-<<<<<<< HEAD
  			port map (cs=>'1', oe=>'1', we=>regwr_out,addr=>mux0, din=>busW_out, dout=>A);
 
 	sram1_map:  	sram	 
 			generic map (mem_file => "reg_init.dat")
 			port map (cs=>'1', oe=>'1', we=>regwr_out,addr=>mux1, din=>busW_out, dout=>B);
-=======
- 			port map (cs=>'1', oe=>'1', we=>regwr,addr=>mux0, din=>input, dout=>KKKKK);
 
-	sram1_map:  	sram	 
-			generic map (mem_file => "reg_init.dat")
-			port map (cs=>'1', oe=>'1', we=>regwr,addr=>mux1, din=>input, dout=>PPPPP);
-        B <= PPPPP;
->>>>>>> 1287bebcec77da95fbd8f87974cf3d1b5fe9c333
-	
 end architecture structural;
--- 6_datapath.vhd
+++ 6_datapath.vhd
@@ -23,17 +23,17 @@ architecture structural of datapath_6 is
             );
     end component;
 
-    component sram	is   
+    component sram	is
     generic(
-	mem_file : string
+mem_file : string
   );
   port (
-	cs	  : in	std_logic;
-	oe	  :	in	std_logic;
-	we	  :	in	std_logic;
-	addr  : in	std_logic_vector(31 downto 0);
-	din	  :	in	std_logic_vector(31 downto 0);
-	dout  :	out std_logic_vector(31 downto 0)
+cs	 : in	std_logic;
+oe	 :	in	std_logic;
+we	 :	in	std_logic;
+addr  : in	std_logic_vector(31 downto 0);
+din	 :	in	std_logic_vector(31 downto 0);
+dout  :	out std_logic_vector(31 downto 0)
   );
    end component;
                    
@@ -55,25 +55,24 @@ architecture structural of datapath_6 is
         clk   : in std_logic;
         regwr        : in  std_logic;
         RegDst : in std_logic; 
-        rs 		 : in std_logic_vector(4 downto 0);
-        rt 		 : in std_logic_vector(4 downto 0);
-        rd 		 : in std_logic_vector(4 downto 0);
-        input	 : in std_logic_vector(31 downto 0);
-        A   	 : out std_logic_vector(31 downto 0);
-        B  		 : out std_logic_vector(31 downto 0)
+        rs : in std_logic_vector(4 downto 0);
+        rt : in std_logic_vector(4 downto 0);
+        rd : in std_logic_vector(4 downto 0);
+        input	: in std_logic_vector(31 downto 0);
+        A   : out std_logic_vector(31 downto 0);
+        B  	: out std_logic_vector(31 downto 0)
       );
     end component;
 
     component mux_32 is
       port (
-	    sel   : in  std_logic;
-	    src0  : in  std_logic_vector(31 downto 0);
-	    src1  : in  std_logic_vector(31 downto 0);
-	    z	    : out std_logic_vector(31 downto 0)
+   sel   : in  std_logic;
+   src0  : in  std_logic_vector(31 downto 0);
+   src1  : in  std_logic_vector(31 downto 0);
+   z	   : out std_logic_vector(31 downto 0)
       );
     end component;
    
-<<<<<<< HEAD
    component datamem is
        port (
            clk : in std_logic;
@@ -84,31 +83,11 @@ architecture structural of datapath_6 is
            );
    end component;
  
-=======
-   signal AND_RegWr, AND_MemWr: std_logic;
-   signal Rw: std_logic_vector ( 4 downto 0);
->>>>>>> 1287bebcec77da95fbd8f87974cf3d1b5fe9c333
    signal busA, busB, busW, busW_out, busB_In, imm_extended, Data_Out, ALU_Output : std_logic_vector (31 downto 0);
    
    begin 
    
-<<<<<<< HEAD
    Register_Memory: reg_32 port map ( clk, RegWr, RegDst, Rs, Rt, Rd, busW, busA, busB);
-=======
-
-   syncreg: sync_s port map (clk, busW, busW_out);
-   
-   Gated_RegWr: and_gate port map(RegWr, clk, AND_RegWr);
-
-   Gated_MemWr: and_gate port map(MemWr, clk, AND_MemWr);
-   
---   the_master: dffr_a port map( clk, RegWr_not, '0', '0', RegWr, '1', master_RegWr); 
---   the_master2: dffr_a port map (clk, MemWr_not, '0', '0', MemWr, '1', master_MemWr);
-   
-   mux_rs_rt_rw: mux_5_S port map ( RegDst, Rt, Rd, Rw);
-   
-   Register_Memory: reg_32 port map ( AND_RegWr, Rw, Rs, Rt, busW_out, busA, busB);
->>>>>>> 1287bebcec77da95fbd8f87974cf3d1b5fe9c333
    
    extender_datapath: extender port map ( ExtOp, imm16, imm_extended);
    
@@ -117,9 +96,7 @@ architecture structural of datapath_6 is
    alu_map: alu port map ( ALUctr, busA, busB_In, cout, ovf, Equal, ALU_Output);
   
    Data_Memory:  	datamem	port map (clk, MemWr, ALU_Output, busB, Data_Out);
- 			 
+ 	 
    Mem_to_reg_mux: mux_32 port map ( MemtoReg, ALU_Output, Data_Out, busW);
 
-end structural;
-
-
+end structural; 
